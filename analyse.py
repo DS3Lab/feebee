a0a48a0dd324fa8eb4b7690588a3ff260c0bac32
Cedric Renggli <cedric.renggli@gmail.com>
2021-07-27 13:16:24 +0200

    flags for analyse script

--- analyse.py
+++ analyse.py
@@ -6,12 +6,16 @@ import pandas as pd
 
 FLAGS = flags.FLAGS
 
+flags.DEFINE_string("path", "outputs", "Path to the matrices directory")
+flags.DEFINE_string("output_file", None, "Output file. None to not store results")
+flags.DEFINE_bool("cleanup", False, "Cleanup LSF files with no matching CSV file")
+
 LSF_PREFIX = "_lsf."
 CSV_SUFFIX = ".csv"
 
 def main(argv):
 
-    base_path = "outputs/"
+    base_path = FLAGS.path
 
     columns = ["dataset", "method", "split", "identifier", "status"]
     results = []
@@ -43,6 +47,9 @@ def main(argv):
                             with open(os.path.join(path, f)) as myfile:
                                 if "TERM_RUNLIMIT: job killed after reaching LSF run time limit." in myfile.read():
                                     suffix[identifier] = "TIMEOUT"
+                            if FLAGS.cleanup:
+                                logging.log(logging.INFO, f"Removing file {f} in {path}.")
+                                os.remove(os.path.join(path, f))
                         else:
                             suffix[identifier] = "OK"
                     elif f.endswith(CSV_SUFFIX):
@@ -58,8 +65,9 @@ def main(argv):
 
                     results.append([dataset, method, split, identifier, suffix[identifier]])
 
-    df = pd.DataFrame(data=results, columns=columns)
-    df.to_csv("results_overview.csv")
+    if FLAGS.output_file:
+        df = pd.DataFrame(data=results, columns=columns)
+        df.to_csv(FLAGS.output_file)
 
 if __name__ == "__main__":
     app.run(main)
